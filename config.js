5da63415a4cf80d27904260894c6428ded1f2347
Eachikty <59160723@go.buu.ac.th>
2019-10-19 16:30:55 +0700

    Authentication but not success because have error pls

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    port: process.env.PORT,
+    jwtKey: process.env.JWT_KET,
+    MONGODB_URL: process.env.MONGODB_URL
+
+}
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const mongodb = require('mongodb')
 const bcrypt = require('bcryptjs')
 
-const app = express()
-const port = process.env.PORT
-const MONGODB_URL = process.env.MONGODB_URL
-const MongoClient = mongodb.MongoClient
+const jwt = require('jsonwebtoken')
+const { port, jwtKey } = require('./config')
 
+const auth = require('./auth')
+const app = express()
+    // const port = process.env.PORT
+    // const MONGODB_URL = process.env.MONGODB_URL
+    // const MongoClient = mongodb.MongoClient
+    // const jwtKey = process.env.JWT_KEY
 app.use(express.json())
 
 
@@ -66,11 +70,45 @@ app.post('/sign-in', async(req, res) => {
         res.status(401).json({ error: `Username.Password is not match` })
         return
     }
-    res.status(200).json({ token: '123456789' })
+
+    let token = await jwt.sign({ email: user.email, id: user._id }, jwtKey, { expiresIn: 30 })
+    res.status(200).json({ token: token })
+})
+app.get('/me', auth, async(req, res) => {
+
+    let decoded = req.decoded
+    const client = await require('./db')
+    let db = client.db('buu')
+    let user = await db.collection('users').findOne({ _id: mongodb.ObjectID(decoded.id) })
+        .catch((err) => {
+            console.error(`Cannot get user by id in /me ${err}`)
+            res.status(500).send({ error: err })
+        })
+    if (!user) {
+        res.status(401).json({ error: `User was not found` })
+        return
+    }
+    delete user.password
+    res.json(user)
+
+
 
 })
+app.put('/me', auth, async(req, res) => {
+    let decoded = req.decoded
+    console.log(decoded);
 
+    let newEmail = req.body.email
+    const client = await require('./db')
+    let db = client.db('buu')
+    let result = await db.collection('users').updateOne({ _id: mongodb.ObjectID(decoded.id) }, { $set: { email: newEmail } })
+        .catch((err) => {
+            console.error(`Cannot get user by id in /me ${err}`)
+            res.status(500).send({ error: err })
+        })
+    res.sendStatus(204)
 
+})
 
 app.listen(port, () => {
     console.log(`Pokemon api started at port ${port}`)
